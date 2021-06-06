d47742e9af40bd69aa1bd3057576037744f4605e
Diego Martins Duarte <diegomartins836@gmail.com>
2021-06-05 21:45:36 -0300

    Criado interface titulos

--- src/main/java/com/br/cobranca/controller/TituloController.java
+++ src/main/java/com/br/cobranca/controller/TituloController.java
@@ -1,14 +1,32 @@
 package com.br.cobranca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.br.cobranca.model.Titulo;
+import com.br.cobranca.repository.Titulos;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 	
-	@RequestMapping("/titulos/novo")
+	@Autowired
+	private Titulos titulos;
+	
+	@RequestMapping("/novo")
 	public String novo() {
 		return "CadastroTitulo";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		
+		titulos.save(titulo);
+		
+		return "CadastroTitulo";
+		
 	}
 
 }
---
+++ src/main/java/com/br/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.br.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{
+
+}
